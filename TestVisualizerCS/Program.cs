2654ceb688b35a2602741fbb7c47d1096296936a
Aleksander Berus <a.berus@gmail.com>
2018-03-09 12:57:15 +0100

    Added VB.NET test project

---
+++ TestVisualizerCS/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+namespace TestVisualizer
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var image1 = System.Drawing.Image.FromFile("tumblr_mwiiixNSpW1qbkusho1_1280.png");
+
+            var visualizerHost = new VisualizerDevelopmentHost(image1, typeof(ImageVisualizer.Visualizer));
+            visualizerHost.ShowVisualizer();
+            var debuggeeObject = visualizerHost.DebuggeeObject;
+
+            Debugger.Break();
+
+            var image2 = new System.Windows.Media.Imaging.BitmapImage();
+            image2.BeginInit();
+            image2.UriSource = new Uri("tumblr_mwiiixNSpW1qbkusho1_1280.png", UriKind.Relative);
+            image2.EndInit();
+
+            visualizerHost = new VisualizerDevelopmentHost(image2, typeof(ImageVisualizer.Visualizer), typeof(ImageVisualizer.ImageVisualizerObjectSource));
+            visualizerHost.ShowVisualizer();
+
+            Debugger.Break();
+
+            var image2small = new System.Windows.Media.Imaging.BitmapImage(); //new Uri("VisualStudio256_256.png", UriKind.Relative));
+            image2small.BeginInit();
+            image2small.StreamSource = new FileStream("VisualStudio256_256.png", FileMode.Open);
+            image2small.EndInit();
+
+            visualizerHost = new VisualizerDevelopmentHost(image2small, typeof(ImageVisualizer.Visualizer), typeof(ImageVisualizer.ImageVisualizerObjectSource));
+            visualizerHost.ShowVisualizer();
+
+            Debugger.Break();
+        }
+    }
+}
---
+++ TestVisualizerVB/Module1.vb
@@ -0,0 +1,40 @@
+﻿Imports System
+Imports System.Diagnostics
+Imports System.IO
+Imports Microsoft.VisualStudio.DebuggerVisualizers
+
+Module Module1
+
+    Sub Main()
+
+        Dim image1 = System.Drawing.Image.FromFile("tumblr_mwiiixNSpW1qbkusho1_1280.png")
+
+        Dim visualizerHost = New VisualizerDevelopmentHost(image1, GetType(ImageVisualizer.Visualizer))
+        visualizerHost.ShowVisualizer()
+        Dim debuggeeObject = visualizerHost.DebuggeeObject
+
+        Debugger.Break()
+
+        Dim image2 = New System.Windows.Media.Imaging.BitmapImage()
+        image2.BeginInit()
+        image2.UriSource = New Uri("tumblr_mwiiixNSpW1qbkusho1_1280.png", UriKind.Relative)
+        image2.EndInit()
+
+        visualizerHost = New VisualizerDevelopmentHost(image2, GetType(ImageVisualizer.Visualizer), GetType(ImageVisualizer.ImageVisualizerObjectSource))
+        visualizerHost.ShowVisualizer()
+
+        Debugger.Break()
+
+        Dim image2small = New System.Windows.Media.Imaging.BitmapImage() 'new Uri("VisualStudio256_256.png", UriKind.Relative));
+        image2small.BeginInit()
+        image2small.StreamSource = New FileStream("VisualStudio256_256.png", FileMode.Open)
+        image2small.EndInit()
+
+        visualizerHost = New VisualizerDevelopmentHost(image2small, GetType(ImageVisualizer.Visualizer), GetType(ImageVisualizer.ImageVisualizerObjectSource))
+        visualizerHost.ShowVisualizer()
+
+        Debugger.Break()
+
+    End Sub
+
+End Module
