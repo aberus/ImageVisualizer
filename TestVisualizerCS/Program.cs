b8288609f6df82a892bc4a236ddab78bd1aa2be1
Aleksander Berus <a.berus@gmail.com>
2020-01-25 17:45:39 +0100

    Update tests that work in VS2019

--- TestVisualizerCS/Program.cs
+++ TestVisualizerCS/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Windows.Media;
-using Microsoft.VisualStudio.DebuggerVisualizers;
 
 namespace TestVisualizer
 {
@@ -12,46 +10,32 @@ namespace TestVisualizer
         static void Main()
         {
             var image1 = System.Drawing.Image.FromFile("tumblr_mwiiixNSpW1qbkusho1_1280.png");
-
-            var visualizerHost = new VisualizerDevelopmentHost(image1, typeof(ImageVisualizer.Visualizer));
-            visualizerHost.ShowVisualizer();
-            var debuggeeObject = visualizerHost.DebuggeeObject;
-
+            ImageVisualizer.Visualizer.TestShowVisualizer(image1);
             Debugger.Break();
 
             var image2 = new System.Windows.Media.Imaging.BitmapImage();
             image2.BeginInit();
             image2.UriSource = new Uri("tumblr_mwiiixNSpW1qbkusho1_1280.png", UriKind.Relative);
             image2.EndInit();
-
-            visualizerHost = new VisualizerDevelopmentHost(image2, typeof(ImageVisualizer.Visualizer), typeof(ImageVisualizer.ImageVisualizerObjectSource));
-            visualizerHost.ShowVisualizer();
-
+            ImageVisualizer.Visualizer.TestShowVisualizer(image2);
             Debugger.Break();
 
             var image3 = new System.Windows.Media.Imaging.BitmapImage(); //new Uri("VisualStudio256_256.png", UriKind.Relative));
             image3.BeginInit();
             image3.StreamSource = new FileStream("VisualStudio256_256.png", FileMode.Open);
             image3.EndInit();
-
-            visualizerHost = new VisualizerDevelopmentHost(image3, typeof(ImageVisualizer.Visualizer), typeof(ImageVisualizer.ImageVisualizerObjectSource));
-            visualizerHost.ShowVisualizer();
-
+            ImageVisualizer.Visualizer.TestShowVisualizer(image3);
             Debugger.Break();
 
-            var pixelFormat = PixelFormats.Bgr32;
+            var pixelFormat = System.Windows.Media.PixelFormats.Bgr32;
             int width = 1280;
             int height = 720;
             int rawStride = (width * pixelFormat.BitsPerPixel + 7) / 8;
             byte[] rawImage = new byte[rawStride * height];
-
-            Random value = new Random();
+            var value = new Random();
             value.NextBytes(rawImage);
-
             var image4 = System.Windows.Media.Imaging.BitmapSource.Create(width, height, 96, 96, pixelFormat, null, rawImage, rawStride);
-            visualizerHost = new VisualizerDevelopmentHost(image4, typeof(ImageVisualizer.Visualizer), typeof(ImageVisualizer.ImageVisualizerObjectSource));
-            visualizerHost.ShowVisualizer();
-
+            ImageVisualizer.Visualizer.TestShowVisualizer(image4);
             Debugger.Break();
         }
     }
--- TestVisualizerVB/Module1.vb
+++ TestVisualizerVB/Module1.vb
@@ -1,56 +1,37 @@
-﻿Imports System
-Imports System.Diagnostics
-Imports System.IO
-Imports System.Windows.Media
-Imports Microsoft.VisualStudio.DebuggerVisualizers
+﻿Imports System.IO
 
 Module Module1
 
     Sub Main()
 
         Dim image1 = System.Drawing.Image.FromFile("tumblr_mwiiixNSpW1qbkusho1_1280.png")
-
-        Dim visualizerHost = New VisualizerDevelopmentHost(image1, GetType(ImageVisualizer.Visualizer))
-        visualizerHost.ShowVisualizer()
-        Dim debuggeeObject = visualizerHost.DebuggeeObject
-
+        ImageVisualizer.Visualizer.TestShowVisualizer(image1)
         Debugger.Break()
 
         Dim image2 = New System.Windows.Media.Imaging.BitmapImage()
         image2.BeginInit()
         image2.UriSource = New Uri("tumblr_mwiiixNSpW1qbkusho1_1280.png", UriKind.Relative)
         image2.EndInit()
-
-        visualizerHost = New VisualizerDevelopmentHost(image2, GetType(ImageVisualizer.Visualizer), GetType(ImageVisualizer.ImageVisualizerObjectSource))
-        visualizerHost.ShowVisualizer()
-
+        ImageVisualizer.Visualizer.TestShowVisualizer(image2)
         Debugger.Break()
 
         Dim image3 = New System.Windows.Media.Imaging.BitmapImage() 'new Uri("VisualStudio256_256.png", UriKind.Relative));
         image3.BeginInit()
         image3.StreamSource = New FileStream("VisualStudio256_256.png", FileMode.Open)
         image3.EndInit()
-
-        visualizerHost = New VisualizerDevelopmentHost(image3, GetType(ImageVisualizer.Visualizer), GetType(ImageVisualizer.ImageVisualizerObjectSource))
-        visualizerHost.ShowVisualizer()
-
+        ImageVisualizer.Visualizer.TestShowVisualizer(image3)
         Debugger.Break()
 
-        Dim pixelFormat = PixelFormats.Bgr32
+        Dim pixelFormat = System.Windows.Media.PixelFormats.Bgr32
         Dim width As Integer = 1280
         Dim height As Integer = 720
         Dim rawStride As Integer = (width * pixelFormat.BitsPerPixel + 7) / 8
         Dim rawImage As Byte() = New Byte(rawStride * height - 1) {}
-
         Dim value As Random = New Random()
         value.NextBytes(rawImage)
-
         Dim image4 = System.Windows.Media.Imaging.BitmapSource.Create(width, height, 96, 96, pixelFormat, Nothing, rawImage, rawStride)
-        visualizerHost = New VisualizerDevelopmentHost(image4, GetType(ImageVisualizer.Visualizer), GetType(ImageVisualizer.ImageVisualizerObjectSource))
-        visualizerHost.ShowVisualizer()
-
+        ImageVisualizer.Visualizer.TestShowVisualizer(image4)
         Debugger.Break()
-
 
     End Sub
 
