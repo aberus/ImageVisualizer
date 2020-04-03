ce9b57d3fadabd7289f4ff282a51ee3bd21d09cf
Aleksander Berus <a.berus@gmail.com>
2020-04-03 10:18:23 +0200

    Added test folder

---
+++ test/TestVisualizerCS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TestVisualizer
+{
+    class Program
+    {
+        [STAThread]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value")]
+        static void Main()
+        {
+            var image1 = System.Drawing.Image.FromFile("tumblr_mwiiixNSpW1qbkusho1_1280.png");
+            //Debugger.Break();
+
+            var image2 = new System.Windows.Media.Imaging.BitmapImage();
+
+            image2.BeginInit();
+            image2.UriSource = new Uri("tumblr_mwiiixNSpW1qbkusho1_1280.png", UriKind.Relative);
+            image2.EndInit();
+            //Debugger.Break();
+
+            var image3 = new System.Windows.Media.Imaging.BitmapImage();
+
+            image3.BeginInit();
+            image3.StreamSource = new FileStream("VisualStudio256_256.png", FileMode.Open);
+            image3.EndInit();
+            //Debugger.Break();
+
+            var pixelFormat = System.Windows.Media.PixelFormats.Bgr32;
+            int width = 1280;
+            int height = 720;
+            int rawStride = (width * pixelFormat.BitsPerPixel + 7) / 8;
+            byte[] rawImage = new byte[rawStride * height];
+            var value = new Random();
+            value.NextBytes(rawImage);
+
+            var image4 = System.Windows.Media.Imaging.BitmapSource.Create(width, height, 96, 96, pixelFormat, null, rawImage, rawStride);
+            Debugger.Break();
+        }
+    }
+}
---
+++ test/TestVisualizerCSCore22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace TestVisualizer
+{
+    class Program
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value")]
+        static void Main(string[] args)
+        {
+            var image1 = System.Drawing.Image.FromFile("tumblr_mwiiixNSpW1qbkusho1_1280_1.png");
+
+
+            var aa = image1.GetType().GetCustomAttributes(typeof(ISerializable), false);
+            var bb = image1.GetType().GetCustomAttributes(typeof(ISerializable), true);
+
+            if (typeof(ISerializable).IsAssignableFrom(image1.GetType()))
+            { 
+            }
+
+            bool i = image1 is ISerializable;
+
+
+            Debugger.Break();
+        }
+    }
+}
