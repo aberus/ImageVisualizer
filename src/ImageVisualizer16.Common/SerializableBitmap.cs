ed149c8421ed587a9d074496d155621313904d77
Aleksander Berus <a.berus@gmail.com>
2020-10-09 16:27:22 +0200

    SerializableBitmap update

--- src/ImageVisualizer16.Common/SerializableBitmap.cs
+++ src/ImageVisualizer16.Common/SerializableBitmap.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Drawing;
-using System.Reflection.Emit;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 
-namespace ImageVisualizer
+namespace Aberus.VisualStudio.Debugger.ImageVisualizer
 {
     [Serializable]
     public class SerializableBitmap : ISerializable
     {
         private Bitmap image;
+        private readonly string expression;
+        public Bitmap Image { get; private set; }
 
         public SerializableBitmap(Bitmap image)
         {
@@ -17,78 +21,93 @@ namespace ImageVisualizer
 
         private SerializableBitmap(SerializationInfo info, StreamingContext context)
         {
-            var ci = image.GetType().GetConstructor(new Type[] { typeof(SerializationInfo), typeof(StreamingContext) });
-            if (ci != null)
+            //byte[] buffer = (byte[])info.GetValue("Data", typeof(byte[]));
+            //try
+            //{
+            //    this.SetNativeImage(this.InitializeFromStream(new MemoryStream(buffer)));
+            //}
+            foreach (var i in info)
             {
-                ci.Invoke(new object[] { info, context });
+                if (string.Equals(i.Name, "Image", StringComparison.OrdinalIgnoreCase))
+                {
+                    try
+                    {
+                        if (i.Value is byte[] array)
+                        {
+                            var stream = new MemoryStream(array);
+                            stream.Seek(0, SeekOrigin.Begin);
+
+                            Image = new Bitmap(stream);
+
+                            //using (MemoryStream mStream = new MemoryStream())
+                            //{
+                            //    mStream.Write(blob, 0, blob.Length);
+                            //    mStream.Seek(0, SeekOrigin.Begin);
+
+                            //    Bitmap bm = new Bitmap(mStream);
+                            //    return bm;
+                            //}
+
+                        }
+                    }
+                    catch (ExternalException)
+                    {
+                    }
+                    catch (ArgumentException)
+                    {
+                    }
+                    catch (OutOfMemoryException)
+                    {
+                    }
+                    catch (InvalidOperationException)
+                    {
+                    }
+                    catch (NotImplementedException)
+                    {
+                    }
+                    catch (FileNotFoundException)
+                    {
+                    }
+                }
+                else if (string.Equals(i.Name, "Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    expression = (string)i.Value;
+                }
             }
         }
 
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        public static implicit operator SerializableBitmap(Bitmap bitmapImage)
         {
-            var mi = image.GetType().GetMethod(nameof(GetObjectData), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-            if (mi != null)
+            return new SerializableBitmap(bitmapImage);
+        }
+
+        public static implicit operator Bitmap(SerializableBitmap serializableBitmapImage)
+        {
+            return serializableBitmapImage.Image;
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            var source = image;
+
+            if (source != null)
             {
-                InvokeNotOverride(mi, image, new object[] { info, context });
+                using (var memoryStream = new MemoryStream())
+                {
+                    source.Save(memoryStream, ImageFormat.Png);
+                    info.AddValue("Image", memoryStream.ToArray(), typeof(byte[]));
+                }
+
+                info.AddValue("Name", source.ToString());
             }
         }
 
-        public static object InvokeNotOverride(System.Reflection.MethodInfo methodInfo, object targetObject, params object[] arguments)
+        public override string ToString()
         {
-            var parameters = methodInfo.GetParameters();
+            if (!string.IsNullOrEmpty(expression))
+                return expression;
 
-            if (parameters.Length == 0)
-            {
-                if (arguments != null && arguments.Length != 0)
-                    throw new Exception("Arguments cont doesn't match");
-            }
-            else
-            {
-                if (parameters.Length != arguments.Length)
-                    throw new Exception("Arguments cont doesn't match");
-            }
-
-            Type returnType = null;
-            if (methodInfo.ReturnType != typeof(void))
-            {
-                returnType = methodInfo.ReturnType;
-            }
-
-            var type = targetObject.GetType();
-            var dynamicMethod = new DynamicMethod("", returnType, new Type[] { type, typeof(object) }, type);
-
-            var iLGenerator = dynamicMethod.GetILGenerator();
-            iLGenerator.Emit(OpCodes.Ldarg_0); // this
-
-            for (int i = 0; i < parameters.Length; i++)
-            {
-                var parameter = parameters[i];
-
-                iLGenerator.Emit(OpCodes.Ldarg_1); // load array argument
-
-                // get element at index
-                iLGenerator.Emit(OpCodes.Ldc_I4_S, i); // specify index
-                iLGenerator.Emit(OpCodes.Ldelem_Ref); // get element
-
-                var parameterType = parameter.ParameterType;
-                if (parameterType.IsPrimitive)
-                {
-                    iLGenerator.Emit(OpCodes.Unbox_Any, parameterType);
-                }
-                else if (parameterType == typeof(object))
-                {
-                    // do nothing
-                }
-                else
-                {
-                    iLGenerator.Emit(OpCodes.Castclass, parameterType);
-                }
-            }
-
-            iLGenerator.Emit(OpCodes.Call, methodInfo);
-            iLGenerator.Emit(OpCodes.Ret);
-
-            return dynamicMethod.Invoke(null, new object[] { targetObject, arguments });
+            return base.ToString();
         }
     }
 }
